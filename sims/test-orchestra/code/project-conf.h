b775e42b331ca5b0b6046ca849bb4470e10deaad
Andreas Urke <arurke@gmail.com>
2020-03-20 00:13:43 +0100

    Optimize node project-conf for RAM and ROM size

--- sims/test-orchestra/code/project-conf.h
+++ sims/test-orchestra/code/project-conf.h
@@ -1,7 +1,7 @@
 #ifndef PROJECT_CONF_H_
 #define PROJECT_CONF_H_
 
-#define LOG_CONF_LEVEL_MAC            LOG_LEVEL_INFO
+#define LOG_CONF_LEVEL_MAC            LOG_LEVEL_ERR
 //#define LOG_CONF_LEVEL_TCPIP          LOG_LEVEL_DBG
 //#define LOG_CONF_LEVEL_IPV6           LOG_LEVEL_DBG
 //#define LOG_CONF_LEVEL_RPL            LOG_LEVEL_DBG
@@ -9,8 +9,11 @@
 //#define LOG_CONF_LEVEL_FRAMER         LOG_LEVEL_INFO
 #define LOG_CONF_WITH_COMPACT_ADDR    1
 
-// Buffer size
+// Total queue buffer size
 #define QUEUEBUF_CONF_NUM             8
+
+// Max queue size per neighbor
+#define TSCH_QUEUE_CONF_NUM_PER_NEIGHBOR   8
 
 // Disable use of pending bit which allows nodes to use more slots
 #define TSCH_CONF_BURST_MAX_LEN       0
@@ -38,5 +41,16 @@
 
 // Sender or receiver based (def. receiver)
 #define ORCHESTRA_CONF_UNICAST_SENDER_BASED   1
+
+
+/* Reduce ROM and RAM usage so that it builds on z1 */
+// No need for large packet support
+#define UIP_CONF_BUFFER_SIZE                  250
+
+// Disable support for packet fragmentation
+#define SICSLOWPAN_CONF_FRAG                  0
+
+// No need for process names (used for debugging)
+#define PROCESS_CONF_NO_PROCESS_NAMES         1
 
 #endif /* PROJECT_CONF_H_ */
