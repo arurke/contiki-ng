eabd04a989cab69ea1d616f7ac924dc6d2fa7405
Andreas Urke <arurke@gmail.com>
2020-03-27 03:33:44 +0100

    DS6: Add route_update to ds6-route notifications

--- os/net/ipv6/uip-ds6-route.c
+++ os/net/ipv6/uip-ds6-route.c
@@ -134,7 +134,7 @@ assert_nbr_routes_list_sane(void)
 #if UIP_DS6_NOTIFICATIONS
 static void
 call_route_callback(int event, const uip_ipaddr_t *route,
-                    const uip_ipaddr_t *nexthop)
+                    const uip_ipaddr_t *nexthop, bool update)
 {
   int num;
   struct uip_ds6_notification *n;
@@ -147,7 +147,7 @@ call_route_callback(int event, const uip_ipaddr_t *route,
     } else {
       num = num_routes;
     }
-    n->callback(event, route, nexthop, num);
+    n->callback(event, route, nexthop, num, update);
   }
 }
 /*---------------------------------------------------------------------------*/
@@ -327,6 +327,7 @@ uip_ds6_route_add(const uip_ipaddr_t *ipaddr, uint8_t length,
 #if (UIP_MAX_ROUTES != 0)
   uip_ds6_route_t *r;
   struct uip_ds6_route_neighbor_route *nbrr;
+  bool update = false;
 
   if(LOG_DBG_ENABLED) {
     assert_nbr_routes_list_sane();
@@ -360,6 +361,7 @@ uip_ds6_route_add(const uip_ipaddr_t *ipaddr, uint8_t length,
     LOG_INFO_6ADDR(ipaddr);
     LOG_INFO_(" found, deleting it\n");
 
+    update = true;
     uip_ds6_route_rm(r);
   }
   {
@@ -468,7 +470,7 @@ uip_ds6_route_add(const uip_ipaddr_t *ipaddr, uint8_t length,
   LOG_ANNOTATE("#L %u 1;blue\n", nexthop->u8[sizeof(uip_ipaddr_t) - 1]);
 
 #if UIP_DS6_NOTIFICATIONS
-  call_route_callback(UIP_DS6_NOTIFICATION_ROUTE_ADD, ipaddr, nexthop);
+  call_route_callback(UIP_DS6_NOTIFICATION_ROUTE_ADD, ipaddr, nexthop, update);
 #endif
 
   if(LOG_DBG_ENABLED) {
@@ -537,7 +539,7 @@ uip_ds6_route_rm(uip_ds6_route_t *route)
 
 #if UIP_DS6_NOTIFICATIONS
     call_route_callback(UIP_DS6_NOTIFICATION_ROUTE_RM,
-        &route->ipaddr, uip_ds6_route_nexthop(route));
+        &route->ipaddr, uip_ds6_route_nexthop(route), false);
 #endif
   }
 
@@ -604,6 +606,7 @@ uip_ds6_defrt_t *
 uip_ds6_defrt_add(const uip_ipaddr_t *ipaddr, unsigned long interval)
 {
   uip_ds6_defrt_t *d;
+  bool refresh = false;
 
   if(LOG_DBG_ENABLED) {
     assert_nbr_routes_list_sane();
@@ -630,6 +633,7 @@ uip_ds6_defrt_add(const uip_ipaddr_t *ipaddr, unsigned long interval)
     list_push(defaultrouterlist, d);
   }
   else {
+    refresh = true;
     LOG_INFO("Refreshing default\n");
   }
 
@@ -644,7 +648,7 @@ uip_ds6_defrt_add(const uip_ipaddr_t *ipaddr, unsigned long interval)
   LOG_ANNOTATE("#L %u 1\n", ipaddr->u8[sizeof(uip_ipaddr_t) - 1]);
 
 #if UIP_DS6_NOTIFICATIONS
-  call_route_callback(UIP_DS6_NOTIFICATION_DEFRT_ADD, ipaddr, ipaddr);
+  call_route_callback(UIP_DS6_NOTIFICATION_DEFRT_ADD, ipaddr, ipaddr, refresh);
 #endif
 
 if(LOG_DBG_ENABLED) {
@@ -674,7 +678,7 @@ uip_ds6_defrt_rm(uip_ds6_defrt_t *defrt)
       LOG_ANNOTATE("#L %u 0\n", defrt->ipaddr.u8[sizeof(uip_ipaddr_t) - 1]);
 #if UIP_DS6_NOTIFICATIONS
       call_route_callback(UIP_DS6_NOTIFICATION_DEFRT_RM,
-			  &defrt->ipaddr, &defrt->ipaddr);
+			  &defrt->ipaddr, &defrt->ipaddr, false);
 #endif
       return;
     }
--- os/net/ipv6/uip-ds6-route.h
+++ os/net/ipv6/uip-ds6-route.h
@@ -91,7 +91,8 @@ void uip_ds6_route_init(void);
 typedef void (* uip_ds6_notification_callback)(int event,
 					       const uip_ipaddr_t *route,
 					       const uip_ipaddr_t *nexthop,
-					       int num_routes);
+					       int num_routes,
+					       bool route_update);
 struct uip_ds6_notification {
   struct uip_ds6_notification *next;
   uip_ds6_notification_callback callback;
