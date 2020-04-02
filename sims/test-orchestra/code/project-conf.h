45a1a49b5057a6784342a369b5c7c47d94a531d1
Andreas Urke <arurke@gmail.com>
2020-04-02 04:54:15 +0200

    Update sim project-conf

--- sims/test-orchestra/code/project-conf.h
+++ sims/test-orchestra/code/project-conf.h
@@ -9,20 +9,29 @@
 //#define LOG_CONF_LEVEL_FRAMER         LOG_LEVEL_INFO
 #define LOG_CONF_WITH_COMPACT_ADDR    1
 
+// Use only objective function 0
+#define RPL_CONF_SUPPORTED_OFS        {&rpl_of0}
+#define RPL_CONF_OF_OCP               RPL_OCP_OF0
+//#define RPL_CONF_STATS                1 // Does not print extra
+#define RPL_OF0_CONF_SR               RPL_OF0_FIXED_SR
+
+// z1 sets to 2, but we have seen queue full, so we try force TSCH default 4
+#define TSCH_CONF_MAX_INCOMING_PACKETS    4
+
 // Total queue buffer size
-#define QUEUEBUF_CONF_NUM             4
+#define QUEUEBUF_CONF_NUM                 4
 
 // Max queue size per neighbor
-#define TSCH_QUEUE_CONF_NUM_PER_NEIGHBOR   4
+#define TSCH_QUEUE_CONF_NUM_PER_NEIGHBOR  4
 
 // Disable use of pending bit which allows nodes to use more slots
-#define TSCH_CONF_BURST_MAX_LEN       0
+#define TSCH_CONF_BURST_MAX_LEN           0
 
 // Disable RPL probing which takes up space in queues
-#define RPL_CONF_WITH_PROBING         0
+#define RPL_CONF_WITH_PROBING             0
 
 // Number of channels (1)
-#define TSCH_CONF_DEFAULT_HOPPING_SEQUENCE    TSCH_HOPPING_SEQUENCE_1_1
+#define TSCH_CONF_DEFAULT_HOPPING_SEQUENCE    TSCH_HOPPING_SEQUENCE_2_2
 
 // Length of EB SF (def. 397)
 #define ORCHESTRA_CONF_EBSF_PERIOD            397
@@ -52,5 +61,8 @@
 
 // No need for process names (used for debugging)
 #define PROCESS_CONF_NO_PROCESS_NAMES         1
+
+// Disable stack checker? (it is not running native btw)
+#define STACK_CHECK_CONF_ENABLED              0
 
 #endif /* PROJECT_CONF_H_ */
