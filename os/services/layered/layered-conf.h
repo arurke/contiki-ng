80c17e2361cbec805ad690d8e480dc51be1c1f7e
Andreas Urke <arurke@gmail.com>
2021-08-04 21:30:04 +0200

    Use deployment ID as layered hash

--- os/services/layered/layered-conf.h
+++ os/services/layered/layered-conf.h
@@ -77,7 +77,12 @@
 #ifdef LAYERED_CONF_LINKADDR_HASH
 #define LAYERED_LINKADDR_HASH                     LAYERED_CONF_LINKADDR_HASH
 #else /* ORCHESTRA_CONF_LINKADDR_HASH */
+#if BUILD_WITH_DEPLOYMENT
+#include "services/deployment/deployment.h"
+#define LAYERED_LINKADDR_HASH(addr)               deployment_id_from_lladdr(addr)
+#else
 #define LAYERED_LINKADDR_HASH(addr)               ((addr != NULL) ? (addr)->u8[LINKADDR_SIZE - 1] : -1)
+#endif
 #endif /* ORCHESTRA_CONF_LINKADDR_HASH */
 
 #endif /* __LAYERED_CONF_H__ */
