1ff3a876f13f26e1eac0f6df864d30a448c977ef
Andreas Urke <arurke@gmail.com>
2021-09-16 22:15:02 +0200

    minor reduce logging in layered

--- os/services/layered/layered-multi-channel.c
+++ os/services/layered/layered-multi-channel.c
@@ -450,10 +450,10 @@ new_time_source(const struct tsch_neighbor *old, const struct tsch_neighbor *new
 {
   LOG_INFO("New time source ");
   LOG_INFO_LLADDR(tsch_queue_get_nbr_address(new));
-  LOG_INFO_(". Add an rx cell here?\n"); //TODO
-  LOG_INFO("ASN now is %"PRIu32" so TS should be %lu\n",
-           tsch_current_asn.ls4b, (tsch_current_asn.ls4b % LAYERED_SF_LEN));
-  LOG_INFO("asn-%x.%lx\n", tsch_current_asn.ms1b, tsch_current_asn.ls4b);
+  LOG_INFO_(".\n");
+//  LOG_INFO("ASN now is %"PRIu32" so TS should be %lu\n",
+//           tsch_current_asn.ls4b, (tsch_current_asn.ls4b % LAYERED_SF_LEN));
+//  LOG_INFO("asn-%x.%lx\n", tsch_current_asn.ms1b, tsch_current_asn.ls4b);
 }
 
 static bool
