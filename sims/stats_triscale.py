ab69e4aed86d0218815a9ba7b94144ad9f837b98
Andreas Urke <arurke@gmail.com>
2022-06-01 00:58:16 +0200

    Make meta-timeline for all-scenarios for PRR instead of ETX

--- sims/stats_triscale.py
+++ sims/stats_triscale.py
@@ -1063,8 +1063,9 @@ def meta_stats(scenarios, plots_meta_dir, plots_time_dir):
 
     meta_non_converged_run_etx_timelines(scenarios, plots_time_dir)
 
-    meta_timeline_all_runs(scenarios, "raw_mac_tx_dfs", "transmissions", "ETX", 4, plots_meta_dir)
-    meta_timeline_all_runs(scenarios, "raw_packets_dfs", "latency", "latency", 7, plots_meta_dir)
+    #meta_timeline_all_runs(scenarios, "raw_mac_tx_dfs", "transmissions", "ETX", 4, plots_meta_dir)
+    meta_timeline_all_runs(scenarios, "raw_mac_cell_dfs", "prr", "PRR", 100, plots_meta_dir)
+    meta_timeline_all_runs(scenarios, "raw_packets_dfs", "latency", "latency", 20, plots_meta_dir)
     meta_timeline_all_runs(scenarios, "raw_packets_dfs", "pdr", "PDR", 100, plots_meta_dir)
 
     meta_all_parent_switches(scenarios, plots_meta_dir)
