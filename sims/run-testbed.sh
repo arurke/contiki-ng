ba8abf725d8b3bff1e767700d5e9771943a5e9fd
Andreas Urke <arurke@gmail.com>
2022-06-18 15:43:20 +0200

    Refactor run-testbed iot-lab command retrying and use for all commands

--- sims/run-testbed.sh
+++ sims/run-testbed.sh
@@ -23,6 +23,26 @@ SITE="grenoble"
 IOTLAB="$LOGIN@$SITE.iot-lab.info"
 NGDIR="${HOME}/vizaworkspace/contiki-ng-arurke"
 
+# Retry given command with sleeps in between, exit if all failed
+# From https://unix.stackexchange.com/questions/82598/how-do-i-write-a-retry-logic-in-script-to-keep-retrying-to-run-it-upto-5-times
+function retry_cmd {
+  local n=1
+  local max=20
+  local delay=30
+  while true; do
+    "$@" && break || {
+      if [[ $n -lt $max ]]; then
+        ((n++))
+        echo "Command failed. Attempt $n/$max:"
+        sleep $delay;
+      else
+        echo "The command has failed after $n attempts."
+        exit 1
+      fi
+    }
+  done
+}
+
 #----------------------- CATCH SIGINT -----------------------#
 # For a clean exit from the experiment
 trap ctrl_c INT
@@ -40,30 +60,25 @@ function ctrl_c() {
 #-------------------- LAUNCH EXPERIMENTS --------------------#
 echo "Submitting experiment $EXPNAME"
 
-# Launch the experiment and obtain its ID
-EXPID=$(iotlab-experiment submit -n $EXPNAME -d $DURATION -l $NODES --site-association $SITE,script=serial_script.sh | grep id | cut -d' ' -f6 || exit 1)
+# We had problems with intermittent failures on the iotlab-*** scripts,
+# we therefore use a retry function to increase robustness
 
-# Wait for the experiment to begin. If command fails, try again 20 times
-tries=0
-until [ "$tries" -ge 20 ]
-do
-   iotlab-experiment wait -i $EXPID && break
-   tries=$((tries+1))
-   sleep 30
-done
+# Launch the experiment and obtain its ID.  Retry if it fails.
+launch_cmd="iotlab-experiment submit -n $EXPNAME -d $DURATION -l $NODES --site-association $SITE,script=serial_script.sh"
+SUBMIT_INFO=$(retry_cmd $launch_cmd) || exit 1
+EXPID=$(echo "$SUBMIT_INFO" | grep id | cut -d' ' -f6) || exit 1
 
-# Flash nodes
-echo "Flashing nodes with $FIRMWARE"
-chronic iotlab-node --flash $FIRMWARE -i $EXPID || exit 1
+# Wait for the experiment to begin. Retry if it fails.
+wait_start_cmd="iotlab-experiment wait -i $EXPID"
+retry_cmd $wait_start_cmd
 
-# Wait for experiment termination. If command fails, try again 20 times
-tries=0
-until [ "$tries" -ge 20 ]
-do
-   iotlab-experiment wait -i $EXPID --state Terminated && break
-   tries=$((tries+1))
-   sleep 30
-done
+# Flash nodes. Retry if it fails.
+flash_cmd="chronic iotlab-node --flash $FIRMWARE -i $EXPID"
+retry_cmd $flash_cmd
+
+# Wait for experiment termination. Retry if it fails.
+wait_end_cmd="iotlab-experiment wait -i $EXPID --state Terminated"
+retry_cmd $wait_end_cmd
 
 #----------------------- RETRIEVE LOG -----------------------#
 mkdir $LOGDIR
