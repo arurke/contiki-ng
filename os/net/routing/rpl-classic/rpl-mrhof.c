a70c9c6ede910b8cacc563e804897b34643fbae6
Andreas Urke <arurke@gmail.com>
2022-08-20 16:37:58 +0200

    PR Fix type-O in RPL comments

--- os/net/routing/rpl-classic/rpl-mrhof.c
+++ os/net/routing/rpl-classic/rpl-mrhof.c
@@ -65,9 +65,9 @@
  * (1 = perfect link, more = worse link). As a result, MRHOF will favor
  * good links over short paths. Without this feature, a hop with 50% PRR (ETX=2)
  * is equivalent to two perfect hops with 100% PRR (ETX=1+1=2). With this
- * feature, the former path obtains ETX=2*2=4 and the former ETX=1*1+1*1=2.
+ * feature, the former path obtains ETX=2*2=4 and the latter ETX=1*1+1*1=2.
  *
- * While this feature helps achieve extra relaibility, it also results in
+ * While this feature helps achieve extra reliability, it also results in
  * added churn. In networks with high congestion or poor links, this can lead
  * to poor connectivity due to more parent switches, loops, Trickle resets, etc.
   */
